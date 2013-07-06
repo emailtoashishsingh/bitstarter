a32b9c46f825eb0f5cab69a7883c4ab3560b2364
Ashish Singh <emailtoAshishSingh@gmail.com>
2013-07-06 10:33:19 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require ('fs');
 var express = require('express');
-
+var data = new Buffer(40);
 var app = express.createServer(express.logger());
+data = fs.readFileSync('index.html');
+console.log ("Length of data is", data.length);
+console.log ("The contents of the buffer are", data.toString('utf-8'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

a16ae99b36a6cc8d569544a07d16311a7ec3b98f
Ashish Singh <emailtoAshishSingh@gmail.com>
2013-07-07 11:18:31 +0000

    tadta

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-llvar fs = require ('fs');
+var fs = require ('fs');
 var express = require('express');
 //var data = new Buffer(40);
 
@@ -8,6 +8,8 @@ var app = express.createServer(express.logger());
 //data = fs.readFileSync('index.html');
 //console.log ("Length of data is", data.length);
 //console.log ("The contents of the buffer are", data.toString('utf-8'));
+
+console.log ("The file contents are", index.toString);
 
 app.get('/', function(request, response) {
     resonse.writeHead(200, {'Content Type':'text/plain'});

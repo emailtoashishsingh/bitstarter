27fe27b22645b63ebb7c4f4bf78f79ba0f2fb04d
Ashish Singh <emailtoAshishSingh@gmail.com>
2013-07-11 10:16:43 +0000

    grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,30 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+//        .option('-u, --url <url pointer>', 'url link that needs to be graded')
         .parse(process.argv);
+ //   if (program.url){
+  //      console.log ("URL is", program.url);
+  //      rest.get(program.url)
+  //      .on('complete', function(result){
+  //          checkHtml((html instanceof Error), result);
+  //          });
+
+   //     } else {
+    //        fs.readFile(program.file, checkHtmlFile);
+     //       }
+
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+//console.log(" This is a cmd test");
